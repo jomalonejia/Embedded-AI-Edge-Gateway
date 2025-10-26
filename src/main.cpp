ddde7ecc6c6121adfb2b41166e13c8a1e85521d7
jeff_walker <jo_malone@outlook.com>
2025-10-26 16:49:58 +0800

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule("main", "Main");
+    engine.loadFromModule("EmbeddedAIEdgeGateway", "UI");
 
     return app.exec();
 }
